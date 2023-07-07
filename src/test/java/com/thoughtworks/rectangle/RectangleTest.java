c8dc316c6aa215274c5de1a8e7e6199920abbd86
Subraja Ramu <subrajaramu02@gmail.com>
2023-07-07 14:18:07 +0000

    Refactor. assertion is improved

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.rectangle;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,12 +10,12 @@ import org.junit.jupiter.api.Test;
 
 public class RectangleTest {
     @Test
-    void areaOfRectangleWithLength2AndBreath3Is6(){
+    void shouldReturnAreaOfRectangleIs6WhenLength2AndBreath3(){
         
         Rectangle rectangle = new Rectangle(2,3);
         
         int area = rectangle.calculateArea();
         
-        assertEquals(6,area);
+        assertThat(area,is(equalTo(6)));
     }
 }
